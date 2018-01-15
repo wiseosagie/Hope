8756db2bdc8f29dc0ea2e8a47ca2917ffe22eb6c
wiseosagie <wiseosagie@yahoo.com>
2018-01-14 23:46:51 -0600

    menu section design

---
+++ HopeHealingAfrica/View/GradientView.swift
@@ -0,0 +1,36 @@
+//
+//  GradientView.swift
+//  HopeHealingAfrica
+//
+//  Created by Wise Osagie on 1/14/18.
+//  Copyright © 2018 Wise Osagie. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.8941176471, green: 0.1960784314, blue: 0.2235294118, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.8941176471, green: 0.1960784314, blue: 0.2235294118, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+
+}
