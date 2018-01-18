67ae9341d7a4a13b94d28880cfc67657fe5c2f55
wiseosagie <wiseosagie@yahoo.com>
2018-01-17 18:46:14 -0600

    mongo db setup

--- HopeHealingAfrica/Utilities/Constants.swift
+++ HopeHealingAfrica/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+// type alias for registered user
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL constant
+let BASE_URL = " https://hopehealingafrica.herokuapp.com/"
+let URL_REGITER = "\(BASE_URL)account/register"
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User DEfaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
