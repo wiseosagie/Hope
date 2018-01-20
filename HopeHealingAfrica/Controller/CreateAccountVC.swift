39a81fa4b126b650ccae0741a12be0ba42e129c0
wiseosagie <wiseosagie@yahoo.com>
2018-01-19 21:32:55 -0600

    creating users

--- HopeHealingAfrica/Controller/CreateAccountVC.swift
+++ HopeHealingAfrica/Controller/CreateAccountVC.swift
@@ -16,6 +16,10 @@ class CreateAccountVC: UIViewController {
     
     @IBOutlet weak var userImg: UIImageView!
     
+    // Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,6 +27,9 @@ class CreateAccountVC: UIViewController {
     }
 
     @IBAction func createAcctPressed(_ sender: Any) {
+        guard let name = usernameTxt.text , usernameTxt.text != "" else {
+            return
+        }
         guard let email = emailTxt.text , emailTxt.text != "" else {
             return
         }
@@ -36,7 +43,14 @@ class CreateAccountVC: UIViewController {
                 AuthService.instance.loginUser(email: email, password: pass, completion: {(success) in
                     
                     if success {
-                        print("Logged in user", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print("working")
+                                print(UserDataService.instance.email, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
+//                        print("Logged in user", AuthService.instance.authToken)
                     }
                 })
             }
