bdf07c5ac4590fcea99c7b01e64c41299712678f
wiseosagie <wiseosagie@yahoo.com>
2018-01-24 23:02:36 -0600

    create account and login

--- HopeHealingAfrica/Services/UserDataService.swift
+++ HopeHealingAfrica/Services/UserDataService.swift
@@ -58,4 +58,15 @@ class UserDataService {
         return newUIColor
     }
     
+    func logoutUser() {
+        id = ""
+        avatarName = ""
+        avatarColor = ""
+        email = ""
+        name = ""
+        AuthService.instance.isLoggedIn = false
+        AuthService.instance.userEmail = ""
+        AuthService.instance.authToken = ""
+    }
+    
 }
