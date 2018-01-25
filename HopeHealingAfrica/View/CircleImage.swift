dd5a13b6c9778fb63f7c6005fd241bc81cf2fa98
wiseosagie <wiseosagie@yahoo.com>
2018-01-24 21:04:58 -0600

    avatar bg color

---
+++ HopeHealingAfrica/View/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  HopeHealingAfrica
+//
+//  Created by Wise Osagie on 1/24/18.
+//  Copyright © 2018 Wise Osagie. All rights reserved.
+//
+
+import UIKit
+
+class CircleImage: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
