9895a150727d6f9495f3ecbaaa5285f7da200f8a
wiseosagie <wiseosagie@yahoo.com>
2018-01-27 09:16:09 -0600

    sockets and channels

---
+++ HopeHealingAfrica/Services/SocketService.swift
@@ -0,0 +1,13 @@
+//
+//  SocketService.swift
+//  HopeHealingAfrica
+//
+//  Created by Wise Osagie on 1/27/18.
+//  Copyright © 2018 Wise Osagie. All rights reserved.
+//
+
+import UIKit
+
+class SocketService: NSObject {
+
+}
